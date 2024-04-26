59ba31cd869f8aef5a1bf2e8299ace389db27bc0
WillKelley1 <wkauburn03@gmail.com>
2024-04-26 14:09:16 -0400

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Will Kelley on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
